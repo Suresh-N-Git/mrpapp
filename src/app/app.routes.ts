15b5f1c588e60455b81d0a8af2b1770271115cd9
Suresh-N-Git <suresh.n@outlook.com>
2025-10-21 14:35:23 +0530

    21-10

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+// import { NavBarComponent } from './nav-bar/nav-bar.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: '**', redirectTo: 'login' }
+  // Any invalid which the user tries to access through URL will be sent here.
+  // Any route where the user has no rights will be handled in the app-routing.module and sent to same page
+  // { path: '**', component: PageNotFoundComponent }
+];
+
